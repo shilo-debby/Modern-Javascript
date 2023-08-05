886c1f1411ea6510d6bcf12c4b31d5f73f9c358e
shilo-debby <gdeborahshilo@gmail.com>
2023-08-05 17:39:38 +0200

    Basic Syntax

---
+++ chapter1/sandbox.js
@@ -0,0 +1,44 @@
+//string
+console.log('hello world');
+
+ //string concatination
+
+let firstName='deborah';
+let secondName='shilo';
+
+let fullName= firstName+' '+secondName;
+console.log(fullName);
+
+// getting characters
+console.log(secondName[2]);
+
+//string lenght
+ console.log(fullName.length);
+
+ // string methods
+ console.log(fullName.toUpperCase());
+ let result=fullName.toLowerCase();
+ console.log(result,fullName);
+
+
+ let email=' gdeborahshilo@gmail.com ';
+//console.log(email);
+//let answer =email.slice(1,10);
+//let answer=email.substring(3,10);
+//let answer = email.replace('s','b');
+
+ 
+
+let radius=10;
+let pi=3.14;
+console.log(radius,pi);
+
+// math operators +,-,*,/,**,%;
+console.log(20/2)
+//let answer= pi*radius**2;
+
+let a=10;
+console.log(a/'hello');
+
+
+
